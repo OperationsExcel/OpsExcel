db27f76c2e5f5f7498b93abdc1bbe3142e2e9fd9
unknown <siddharth.joshius@gmail.com>
2025-02-26 12:46:06 +0530

    With Streamlit.Success code added

--- Main.py
+++ Main.py
@@ -1,53 +1,55 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[8]:
 
 
 import streamlit as st
 
 
-# In[2]:
+# In[9]:
 
 
-st.title("First Streamlit Application")
+st.title("Streamlit Cloud Application Demo")
 
 
-# In[3]:
+# In[10]:
 
 
-if st.button("Trial button"):
+if st.button("Control Button"):
+    st.success("Control activation!")
     print("Button activated")
 
 
-# In[4]:
+# In[11]:
 
 
 value1=st.number_input('First input',min_value=1,step=2,max_value=20)
 
 
-# In[ ]:
+# In[12]:
 
 
 value2=st.number_input('Second input',min_value=2,step=3,max_value=30)
 
 if st.button("Total button"):
     print("Total button clicked")
-    print(value1+value2)
+    st.success(value1+value2)
 
 
-# In[9]:
+# In[13]:
 
 
 from Multfunc import multiplyfunction
 
 
-# In[10]:
+# In[16]:
 
 
 if st.button("Function import value button"):
     print("In function import button")
-    print(value1*value2)
+    value3=multiplyfunction(value1,value2)
+    st.success(value3)
 
 
 # In[ ]:
