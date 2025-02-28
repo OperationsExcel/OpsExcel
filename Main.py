94da7af3fd6dfe98851156da5b772115e3eca6fa
unknown <siddharth.joshius@gmail.com>
2025-02-28 13:48:49 +0530

    With regression model

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[3]:
 
 
 import streamlit as st
 
 
-# In[9]:
+# In[4]:
 
 
 st.title("Streamlit Cloud Application Demo")
 
 
-# In[10]:
+# In[5]:
 
 
 if st.button("Control Button"):
@@ -21,34 +21,49 @@ if st.button("Control Button"):
     print("Button activated")
 
 
-# In[11]:
+# In[6]:
 
 
-value1=st.number_input('First input',min_value=1,step=2,max_value=20)
+value1=st.number_input('Area in Square Feet',min_value=650,step=20,max_value=1800)
 
 
-# In[12]:
+# In[7]:
 
 
-value2=st.number_input('Second input',min_value=2,step=3,max_value=30)
+value2=st.number_input('Number of bedrooms',min_value=2,step=1,max_value=3)
 
 if st.button("Total button"):
     print("Total button clicked")
     st.success(value1+value2)
 
 
-# In[13]:
+# In[8]:
 
 
 from Multfunc import multiplyfunction
 
 
-# In[16]:
+# In[9]:
 
 
 if st.button("Function import value button"):
     print("In function import button")
     value3=multiplyfunction(value1,value2)
+    st.success(value3)
+
+
+# In[11]:
+
+
+from Multfunc import cloudregression
+
+
+# In[12]:
+
+
+if st.button("Calculate Value of Property"):
+    print("In function import button")
+    value3=cloudregression(value1,value2)
     st.success(value3)
 
 
